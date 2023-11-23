281e13cb29fc980bcc2b132ffffd9fad88284cda
MaharshiJ88 <136585286+MaharshiJ88@users.noreply.github.com>
2023-11-23 23:28:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	if (!str || typeof str !== 'string') {
+        return null;
+    }
+
+    const charCount = {};
+
+    // Count occurrences of each character
+    for (let char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    // Find the first non-repeated character
+    for (let char of str) {
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+
+    return null; // No non-repeated character found
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
